6dc348181bd368c768a50a12e94f5c8ea456b2c1
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-11-19 17:48:15 +0530

    fix: logo fix

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -15,7 +15,7 @@ const SidebarItem = ({ item, setShowRecharge, toggleSidebar=() => {} }) => {
     return (
         <>
             <div onClick={item.isDropdown ? () => setIsOpen(!isOpen) : (item.name === "Wallet Recharge" ? () => setShowRecharge(true) : () => {navigate(`/dashboard/${item.url}`); toggleSidebar();})} className={` cursor-pointer px-2 w-full h-12 ${isCurrentMenu ? 'bg-red-500' : 'bg-black'} text-white focus:outline-none transition-all duration-300 hover:bg-red-400 relative flex items-center`}>
-                {item.icon !== "/logo-nobg.png" && <item.icon className='mr-3' />}
+                {item.icon !== "/image/logo-nobg.png" && <item.icon className='mr-3' />}
                 <p className=''>{item.name}</p>
                 {item.isDropdown ? <p className={`absolute transition-transform duration-300 ${isOpen ? "rotate-90" : ""} right-1`}>
                     &#9656;
