ad0de7011d1c5b613c1c574e51c7d9e0a05bc846
AdityaKumar5155 <adityakumar5155@gmail.com>
2026-04-04 20:36:59 +0530

    chore: maintenance announcement

--- src/components/AnnoucementCard.jsx
+++ src/components/AnnoucementCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AnnoucementCard = () => {
   return (
     <div className='p-4 bg-red-500 m-5 rounded-md text-white'>
-        SHIPWALE WILL BE IN MAINTENANCE FROM 22-03-2026 03:00 PM TO 22-03-2026 06:00 PM. THANK YOU FOR YOUR COOPERATION.
+        SHIPWALE WILL BE IN MAINTENANCE FROM 04-04-2026 10:00 PM TO 04-04-2026 11:00 PM. THANK YOU FOR YOUR COOPERATION.
     </div>
   )
 }
